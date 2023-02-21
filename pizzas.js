a884c1a0bfaebea7d73d5d4b655bae83a394996b
mhoffmann76 <hoffmannmarcelo@hotmail.com>
2023-02-21 14:26:01 -0300

    Versao 1.0

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,65 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+  {
+    id: 1,
+    name: "Mussarela",
+    img: "images/pizza.png",
+    price: 20.19,
+    sizes: ["100g", "530g", "860g"],
+    description:
+      "Queijo mussarela, farinha de trigo enriquecida com ferro e ácido fólico, polpa de tomate, azeitonas pretas, óleo de soja, fermento biológico, sal, açúcar, azeite de oliva e orégano",
+  },
+  {
+    id: 2,
+    name: "Calabresa",
+    img: "images/pizza2.png",
+    price: 18.0,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "É especialmente preparada com recheio generoso de linguiça calabresa de primeira qualidade, queijo muçarela e orégano. A linguiça calabresa agrega um sabor único e levemente picante à pizza.",
+  },
+  {
+    id: 3,
+    name: "Quatro Queijos",
+    img: "images/pizza3.png",
+    price: 17.45,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+  },
+  {
+    id: 4,
+    name: "Americana",
+    img: "images/pizza4.png",
+    price: 19.77,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+  },
+  {
+    id: 5,
+    name: "Sorvete",
+    img: "images/pizza5.png",
+    price: 21.43,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+  },
+  {
+    id: 6,
+    name: "Moda da Casa",
+    img: "images/pizza6.png",
+    price: 18.55,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+  },
+  {
+    id: 7,
+    name: "Chocolate",
+    img: "images/pizza7.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+  },
 ];
